23d3affa376bcc94853545896ad97da188ac18e2
wan58169 <wan2njupt@163.com>
2023-03-01 14:45:01 +0800

    lab11: networking

--- kernel/proc.h
+++ kernel/proc.h
@@ -82,18 +82,6 @@ struct trapframe {
 
 enum procstate { UNUSED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
-struct vma {
-  uint64 addr;
-  int len;
-  int prot;
-  int flags;
-  int fd;
-  int offset;
-  struct file *file;
-};
-
-#define NVMA 16
-
 // Per-process state
 struct proc {
   struct spinlock lock;
@@ -115,5 +103,4 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  struct vma vmas[NVMA];
 };
